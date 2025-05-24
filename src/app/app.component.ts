663f50cd7577c57c5e0ea92ea8b411d7f0f481b7
Diego <diego.dev.gow@hotmail.com>
2025-05-24 05:10:23 -0300

    criação module para componentes do angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,17 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-import { LayoutModule, MediaMatcher } from '@angular/cdk/layout';
+import { DialogAlertComponent } from './components/dialogs/dialog-alert/dialog-alert.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MaterialModule } from './modules/material.module';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     RouterLink,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatTooltipModule,
-    MatDividerModule,
-    MatBadgeModule,
-    MatSlideToggleModule,
-    MatSelectModule,
-    LayoutModule
+    MaterialModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
